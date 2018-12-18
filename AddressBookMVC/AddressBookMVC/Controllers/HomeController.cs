8615d4b73510771aa970bcba2c02a58d11577c67
Arunkumar Dwaraka <arunkumar.dwaraka@conceptbytes.com>
2018-12-18 23:09:00 +0400

    Initial commit

---
+++ AddressBookMVC/AddressBookMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AddressBookMVC.Models;
+
+namespace AddressBookMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserRegisterInfo userRegister)
+        {
+            return View();
+        }
+    }
+}
---
+++ AddressBookMVC/AddressBookMVC/Models/UserRegisterInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBookMVC.Models
+{
+    public class UserRegisterInfo
+    {
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
